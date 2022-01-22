32d4c0cc3beed1faac35a79420d1f73854a157cf
KonNech <90194251+KonNech@users.noreply.github.com>
2022-01-22 21:42:06 +0300

    Add files via upload

---
+++ src/plugins/config.rb
@@ -0,0 +1,15 @@
+css_dir = "."
+sass_dir = "."
+images_dir = "."
+fonts_dir = "fonts"
+relative_assets = true
+
+output_style = :compact
+line_comments = false
+
+preferred_syntax = :scss
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+St
